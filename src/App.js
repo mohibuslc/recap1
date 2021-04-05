475666af7ac1226524b80e79b7e1daab94d041d7
mohibuslc <mohib_uslc@outlook.com>
2021-04-05 22:35:29 +0100

    recap+33/1=33/12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+    import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import './App.css';
 
-export default App;
+
+    function App(){
+
+      const info =['KALAM','TINA','FRIDA','MONI','NILU'];
+
+      return(
+        <div style={{textAlign:"center"}}>
+        <Friends name ={info[0] } age="89"></Friends>
+        <Friends name ={info[1]}></Friends>
+        <Friends name ={info[2]}></Friends>
+        <Friends name ={info[3]}></Friends>
+        <Friends name = {info[4]}></Friends>
+
+        </div>
+
+      );
+            }
+
+    function Friends(props){
+     console.log(props);
+      const design ={
+
+        margin:'10px',
+        border: '2px solid red',
+        
+        
+      }
+    
+      return (
+      
+        <div style={design}>
+          <h2> Good Morning </h2>
+          <p>{props.name} Hellow !!! i m Start Recap : Module 31 To 48</p>
+          <p>My age is : {props.age || 45}</p>
+          
+          </div>
+      )
+      
+      }
+
+
+    export default App;
